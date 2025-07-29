36abcdd5c5869c3b9408c86d79b95210f7f197ae
Nishigami <nissi.1125.o@gmail.com>
2025-07-30 01:43:30 +0900

    カウンターに機能を追加

--- test_react_next.js_week5/app/Counter.js
+++ test_react_next.js_week5/app/Counter.js
@@ -1,18 +1,27 @@
 "use client";
 import { useState } from "react";//useStateをインポート
 
-
+//ほかのファイルからCounter関数を呼び出し可能になる
 export default function Counter() {
     const [count, setCount] = useState(0);
 
     const incrementCount = () => {
         setCount(count + 1);
     };
+    const decrementCount = () => {
+        setCount(count - 1);
+    };
+    const resetCount=()=>{
+        setCount(0);
+    };
+
 
     return (
         <div>
             <p>現在のカウント： {count}</p>
             <button onClick={incrementCount}>カウントを増やす</button>
+            <button onClick={decrementCount}>カウントを減らす</button>
+            <button onClick={resetCount}>リセット</button>
         </div>
     );
 }
